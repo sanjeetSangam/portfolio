ccae9ab5d1e4d129b8f28d0970ac8979d697ec22
sanjeetSangam <factsbe0@gmail.com>
2022-06-05 15:52:37 +0530

    updated layout

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -37,15 +37,17 @@ const Home = () => {
             Full Stack Web Developer
           </h4>
           <h5 className="about__cont">
-            I love programming and also love to make wonderful web apps. I will
-            love to contribute more in this field and also I will be love to
-            face new challenges in the upcoming days. Apart from coding,{" "}
+            I love to do programming and also love to make exciting web apps. I
+            had completed my B.tech with the stream of Manufacturing Engineering
+            from CIPET college in Ahmedabad. But having a decent interest in
+            computer science brings me to the Web development field and now, I
+            have learnt MERN stack. I wish to work in a{" "}
             <span
               style={{ color: "skyblue", background: "red", padding: "0.2rem" }}
             >
-              Drawing{" "}
-            </span>
-            is my favorite which I like the most.
+              technological environment
+            </span>{" "}
+            and also I will love to face new challenges in the upcoming days.
           </h5>
           <RSbtn />
         </div>
@@ -59,10 +61,21 @@ const Home = () => {
             </div>
 
             <div className="my__about">
-              I love programming and also love to make wonderful web apps. I
-              will love to contribute more in this field and also I will be love
-              to face new challenges in the upcoming days. Apart from coding,
-              Drawing is my favorite which I like the most.
+              I love to do programming and also love to make exciting web apps.
+              I had completed my B.tech with the stream of Manufacturing
+              Engineering from CIPET college in Ahmedabad. But having a decent
+              interest in computer science brings me to the Web development
+              field and now, I have learnt MERN stack. I wish to work in a{" "}
+              <span
+                style={{
+                  color: "skyblue",
+                  background: "red",
+                  padding: "0.2rem",
+                }}
+              >
+                technological environment
+              </span>{" "}
+              and also I will love to face new challenges in the upcoming days.
             </div>
           </div>
         </div>
